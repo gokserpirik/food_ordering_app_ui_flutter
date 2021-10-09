47b83c9ea8908b3471c38fc19103082c3aa72038
Gökser <48176173+gokserpirik@users.noreply.github.com>
2021-10-09 15:54:21 +0300

    Update colors.dart

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 List<Color> colors = [Color.fromRGBO(255, 211, 0, 1), Color.fromRGBO(93, 62, 188, 1),Color.fromRGBO(250, 250, 250, 1),Color.fromRGBO(76, 51, 152, 1),];
-// ! sarı, mor, beyaz, koyu mor
+// ! yellow, purple, white, dark purple
